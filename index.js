5dc833ce8075213d8ce7099d54260826e59c25cf
brunomaria71 <brunomaria71@yahoo.com>
2023-07-10 20:35:52 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Using moment.js - refer to documentation here:
+// https://momentjs.com/
+
+// Question 1. Parsing
+// Given the following strings:
+// "13/02/2022",
+// "03/04/2022",
+// "Q3 of 2021",
+// "Tue, 22 Feb 2022"
+// Write a function to display each date in “standard” mm/dd/yyyy format.
+// function formatAnyInputToStandardDate(input) {
+// }
+
+function parse(date) {
+  console.log("log");
+}
