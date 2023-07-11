4ff8cd37834bc7cc67e51e108fc492eef26e4aee
brunomaria71 <brunomaria71@yahoo.com>
2023-07-11 19:11:37 -0400

    adding last monday of year challenge

--- index.js
+++ index.js
@@ -48,3 +48,21 @@ function getFirstMondayOfYear(year) {
 let thisYear = getFirstMondayOfYear(2023); // should output jan 2 2023
 
 console.log("the first monday of this year is ", thisYear);
+
+// 3. Last Monday of Year
+// Given a year, write a function to get the last Monday of the year. Display the date in
+// mm/dd/yyyy format.
+
+function getLastMondayOfYear(year) {
+  let formattedDate;
+  return (formattedDate = moment()
+    .year(year)
+    .month(11)
+    .endOf("month")
+    .isoWeekday(1)
+    .format("MM-DD-YYYY"));
+}
+
+let lastYear = getLastMondayOfYear(2023); // should output 12/25/2023
+
+console.log("the last monday of this year is ", lastYear);
