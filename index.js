8066af78e196d03ccbe2d223a5dedc06bc00334d
brunomaria71 <brunomaria71@yahoo.com>
2023-07-10 23:04:21 -0400

    adding second challenge

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ import moment from "moment";
 // Question 1. Parsing
 // Given the following strings:
 // Write a function to display each date in “standard” mm/dd/yyyy format.
-// function formatAnyInputToStandardDate(input) {
-// }
 
 function formatAnyInputToStandardDate(input) {
   let formattedDate;
@@ -31,3 +29,22 @@ console.log("the date that was day first is", dayFirst);
 console.log("the normal date is", normalDate);
 console.log("the quarter date is", quarterDate);
 console.log("the date without weekday is", dayOfWeek);
+
+// 2. First Monday of Year
+// Given a year, write a function to get the first Monday of the year. Display the date in
+// mm/dd/yyyy format.
+
+function getFirstMondayOfYear(year) {
+  let formattedDate;
+  return (formattedDate = moment()
+    .year(year) // returns year
+    .month(0) // hardcoding the january month which is 0
+    .startOf("month")
+    .isoWeekday(8) // we want the first monday, of the month. 1 = the monday before which was 1/26. 8 days l8er is the actual first monday of the mo
+    .format("MM-DD-YYYY"));
+  // given year 2023, format first monday (d(1)) month M(1)
+}
+
+let thisYear = getFirstMondayOfYear(2023); // should output jan 2 2023
+
+console.log("the first monday of this year is ", thisYear);
