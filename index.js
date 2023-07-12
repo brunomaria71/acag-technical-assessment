6a6df449503e332fe9bf6d6b90136ff52cb0194a
brunomaria71 <brunomaria71@yahoo.com>
2023-07-11 21:11:30 -0400

    adding difference of time challenge

--- index.js
+++ index.js
@@ -66,3 +66,34 @@ function getLastMondayOfYear(year) {
 let lastYear = getLastMondayOfYear(2023); // should output 12/25/2023
 
 console.log("the last monday of this year is ", lastYear);
+
+// 4. Difference between two dates
+// Given two dates and two time markers, write a function to find the difference between two dates
+// in the following format: "x Years, X Months, X Days, X Hours, and X Minutes"
+// Example:
+// differenceBetweenTwoDates("03/01/2022","13:03", "03/01/2022", "15:04")
+// Result:
+// 0 Years, 0 Months, 0 Days, 2 Hours, and 1 Minute
+function differenceBetweenTwoDatesAndTime(date1, time1, date2, time2) {
+  let start = moment(`${date1} ${time1}`, "MM/DD/YYYY HH:mm");
+  let end = moment(`${date2} ${time2}`, "MM/DD/YYYY HH:mm");
+
+  const duration = moment.duration(end.diff(start));
+
+  const years = duration.years();
+  const months = duration.months();
+  const days = duration.days();
+  const hours = duration.hours();
+  const minutes = duration.minutes();
+
+  const result = `${years} Years, ${months} Months, ${days} Days, ${hours} Hours, and ${minutes} Minutes`;
+  return result;
+}
+
+let difference = differenceBetweenTwoDatesAndTime(
+  "03/01/2022",
+  "13:03",
+  "03/04/2022",
+  "15:04"
+); // should result in 3 days 2 hours 1 min
+console.log("the difference between these dates is", difference);
